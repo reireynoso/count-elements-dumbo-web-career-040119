99be101ffdcd182caa3203b6328e6c8d8cfb6288
reireynoso <reireynoso@gmail.com>
2019-08-21 18:13:25 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,17 @@
+require 'pry'
+
+
 def count_elements(array)
   # code goes here
+  # binding.pry
+  hash = {}
+  array.map do |element|
+    if !hash[element]
+      hash[element] = 1
+    else
+      hash[element] = hash[element] + 1
+    end
+  end
+  return hash
 end
  
